6d1fedf1757695c2eb6a77fa32b7ec37d4c5f912
Aslan4242 <aslan200800>
2023-03-12 17:16:11 +0300

    поправил замечания, удалил неиспользуемый код

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -1,7 +1,10 @@
 package ru.yandex.practicum.contacts.presentation.base;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.DiffUtil;
+
+import ru.yandex.practicum.contacts.presentation.filter.model.FilterContactTypeUi;
 
 public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T> {
     @Override
@@ -12,6 +15,12 @@ public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUt
 
     @Override
     public boolean areContentsTheSame(@NonNull T oldItem, @NonNull T newItem) {
-        return false;
+        return oldItem.equals(newItem);
+    }
+
+    @Nullable
+    @Override
+    public Object getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
+        return newItem;
     }
 }
